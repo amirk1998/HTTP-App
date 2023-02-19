f73c01365e4da56a76635326fa4e93b3f19470d0
amirk1998 <amirk1998.pv@gmail.com>
2023-02-19 12:51:32 +0330

    Comment Component has been updated

--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -5,12 +5,10 @@ const Comment = ({ name, email, onClick }) => {
       className='bg-indigo-200 rounded-lg flex flex-col items-center justify-center w-1/4 p-2 cursor-pointer'
     >
       <div className='flex flex-col items-center justify-center px-4 py-1'>
-        <p>Name </p>
-        <p>{name} </p>
+        <p className='text-sm text-center'>{name} </p>
       </div>
       <div className='flex flex-col items-center justify-center px-4 py-1'>
-        <p>Email </p>
-        <p>{email} </p>
+        <p className='text-sm text-center'>{email} </p>
       </div>
     </div>
   );
